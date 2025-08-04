d3f9de2b9dd3ce38c94267555ec3bc9e7a6f34a7
Aesir77 <134356658+Aesir77@users.noreply.github.com>
2025-08-04 22:03:23 +0800

    wat

---
+++ Assets/Scripts/Enemy/Spawn_Manager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Spawn_Manager : MonoBehaviour
+{
+    [SerializeField] private Transform SpawnPoint;
+    public GameObject RatPrefab;
+    public int no_ofRats = 10;
+    public float spawnRange = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void SpawnRats()
+    {
+        for (int i = 0; i < no_ofRats; i++)
+        {
+            Vector3 spawnPosition = SpawnPoint.position + new Vector3(Random.Range(-spawnRange, spawnRange), 0, Random.Range(-spawnRange, spawnRange));
+            Instantiate(RatPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}
